4b75b23c0e920e156bed27bae39665a31d5fdf2d
Joshua Lara <joshualara0910@gmail.com>
2024-01-11 11:00:27 -0700

    functions

--- 4Day.js
+++ 4Day.js
@@ -1,4 +1,25 @@
 // day 4 coding 
 //lets make a change
 let brick = 400
+//basic function
+// function salary(mysalary, wsalary) {
+//     return mysalary + wsalary
+// }
 
+// console.log(salary(200, 400))
+
+
+//defalut argument
+// function salary(mysalary, wsalary = 100) {
+//     return mysalary + wsalary
+// }
+
+// console.log(salary(200))
+
+
+
+function salary(mysalary, wsalary = 100) {
+    return mysalary + wsalary
+}
+
+console.log(salary(200))
